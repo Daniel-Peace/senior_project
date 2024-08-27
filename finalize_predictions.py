742df76ee5b43bd661cc563a871f94e7dac2f35a
Daniel Peace <danielpeace805@gmail.com>
2024-08-27 15:21:48 -0700

    - removed publihsing code from  finalize_predictions.py - added subscriptiong to model 0  predictions

--- finalize_predictions.py
+++ finalize_predictions.py
@@ -28,10 +28,7 @@
 import rospy
 import time
 import json
-
-# from casualty import Afflication_type as AT
 from casualty import Casualty
-
 from messages.msg import Assigned_apriltag, Critical_report
 from messages.msg import Injury_report
 from messages.msg import Prediction
@@ -71,80 +68,46 @@ model_4_casualty    = Casualty()   # NONE
 model_5_casualty    = Casualty()   # NONE
 finalized_casualty  = Casualty()
 
-# # publishes injury reports
-# def publish_injury_reports():
-#     injury_report = Injury_report()
-#     injury_report.casualty_id   = finalized_casualty.apriltag
-#     injury_report.team          = TEAM_NAME
-#     injury_report.system        = SYSTEM
-#     injury_report.type          = AT.TRAUMA_HEAD
-#     injury_report.value         = finalized_casualty.trauma_head
-#     injury_report_pub.publish(injury_report)
-#     injury_report.type          = AT.TRAUMA_TORSO
-#     injury_report.value         = finalized_casualty.trauma_torso
-#     injury_report_pub.publish(injury_report)
-#     injury_report.type          = AT.TRAUMA_LOWER_EXT
-#     injury_report.value         = finalized_casualty.trauma_lower_ext
-#     injury_report_pub.publish(injury_report)
-#     injury_report.type          = AT.TRAUMA_UPPER_EXT
-#     injury_report.value         = finalized_casualty.trauma_upper_ext
-#     injury_report_pub.publish(injury_report)
-#     injury_report.type          = AT.ALERTNESS_OCULAR
-#     injury_report.value         = finalized_casualty.alertness_ocular
-#     injury_report_pub.publish(injury_report)
-#     injury_report.type          = AT.ALERTNESS_VERBAL
-#     injury_report.value         = finalized_casualty.alertness_verbal
-#     injury_report_pub.publish(injury_report)
-#     injury_report.type          = AT.ALERTNESS_MOTOR
-#     injury_report.value         = finalized_casualty.alertness_motor
-#     injury_report_pub.publish(injury_report)
-
-# # publishes vitals reports
-# def publish_vitals_reports():
-#     vitals_report = Vitals_report()
-#     vitals_report.casualty_id   = finalized_casualty.apriltag
-#     vitals_report.team          = TEAM_NAME
-#     vitals_report.system        = SYSTEM
-#     vitals_report.type          = AT.HEART_RATE
-#     vitals_report.value         = finalized_casualty.heart_rate
-#     vitals_report_pub.publish(vitals_report)
-#     vitals_report.type          = AT.RESPIRATORY_RATE
-#     vitals_report.value         = finalized_casualty.respiratory_rate
-#     vitals_report_pub.publish(vitals_report)
-
-# # publishes critical reports
-# def publish_critical_reports():
-#     critical_report = Critical_report()
-#     critical_report.casualty_id = TEAM_NAME
-#     critical_report.system      = SYSTEM
-#     critical_report.type        = AT.SEVERE_HEMORRHAGE
-#     critical_report.value       = finalized_casualty.severe_hemorrhage
-#     critical_report_pub.publish(critical_report)
-#     critical_report.type        = AT.RESPIRATORY_DISTRESS
-#     critical_report.value       = finalized_casualty.respiratory_distress
-
 # loops until all needed predictions have been received
 def wait_for_predictions():
+    print("system: waiting for predictions...")
     while model_0_has_not_predicted:
+        time.sleep(0.5)
         continue
+    print("system: received model 0 prediction")
 
 # publishes reports to respective topics
 def publish_reports():
     finalized_casualty.publish_reports()
-    # publish_critical_reports()
-    # publish_vitals_reports()
-    # publish_injury_reports()
 
 # handles combining and finalizing reports
 def finalize_afflication_values():
+    set_apriltag()
     wait_for_predictions()
+    finalized_casualty.severe_hemorrhage = model_0_casualty.severe_hemorrhage
+    finalized_casualty.trauma_head = model_0_casualty.trauma_head
+    finalized_casualty.trauma_torso = model_0_casualty.trauma_torso
+    finalized_casualty.trauma_lower_ext = model_0_casualty.trauma_lower_ext
+    finalized_casualty.trauma_upper_ext = model_0_casualty.trauma_upper_ext
 
+# waits for specified models to finish predictions
 def wait_for_apriltag():
+    print("system: waiting for AprilTag...")
     while not received_april:
+        time.sleep(0.5)
         continue
 
 # resets tracking variables
 def reset_trackers():
+    print("system: reseting trackers")
+    global received_april
+    global model_0_has_not_predicted
+    global model_1_has_not_predicted
+    global model_2_has_not_predicted
+    global model_3_has_not_predicted
+    global model_4_has_not_predicted
+    global model_5_has_not_predicted
+
     received_april              = False
     model_0_has_not_predicted   = True
     model_1_has_not_predicted   = True
@@ -155,10 +118,12 @@ def reset_trackers():
 
 # resets apriltag value
 def reset_apriltag():
+    print("system: reseting AprilTag")
     apriltag = -1
 
 # this functions resets all of the afflication values to -1
 def reset_casualty_objects():
+    print("system: reseting casuatly objects")
     model_0_casualty.reset()
     model_1_casualty.reset()
     model_2_casualty.reset()
@@ -170,6 +135,7 @@ def reset_casualty_objects():
 # sets the AprilTag of the current casualty
 def set_apriltag():
     wait_for_apriltag()
+    print("system: setting AprilTag")
     finalized_casualty.apriltag = apriltag
 
 # calls all reset functions
@@ -180,13 +146,14 @@ def reset():
 
 # handles actions that need to take place when a timer finishes
 def on_timer_finish():
+    print("system: timer has ended...")
     finalize_afflication_values()
     publish_reports()
 
 # handles actions that need to take place when a timer starts
 def on_timer_start():
+    print("system: timer has started...")
     reset()
-    set_apriltag()
 
 # handles actions corresponding to the timer starting and stopping
 def handle_timer_status(msg):
@@ -197,10 +164,16 @@ def handle_timer_status(msg):
 
 # gets the assigned AprilTag and sets it
 def assign_apriltag(msg):
+    print("system: received AprilTag")
+    global apriltag
     apriltag = msg.apriltag
+    global received_april
+    received_april = True
+    print("system: set AprilTag")
 
+# callback function for when predictions are received from model 0
 def receive_model_0_predictions(predictions):
-    for prediction in predictions:
+    for prediction in predictions.prediction_elements:
         if prediction.type == Casualty.SEVERE_HEMORRHAGE:
             model_0_casualty.severe_hemorrhage = prediction.value
         elif prediction.type == Casualty.TRAUMA_HEAD:
@@ -211,6 +184,9 @@ def receive_model_0_predictions(predictions):
             model_0_casualty.trauma_lower_ext = prediction.value
         elif prediction.type == Casualty.TRAUMA_UPPER_EXT:
             model_0_casualty.trauma_upper_ext = prediction.value
+    model_0_casualty.print_self()
+    global model_0_has_not_predicted
+    model_0_has_not_predicted = False
 
 if __name__ == "__main__":
     # registering callback functions
@@ -218,7 +194,7 @@ if __name__ == "__main__":
     rospy.Subscriber('assigned_apriltag', Assigned_apriltag, assign_apriltag)
     rospy.Subscriber('model_0_predictions', Prediction, receive_model_0_predictions)
 
-# loops until user kills the program
+    # loops until user kills the program
     while not rospy.is_shutdown():
         # sleeping to slow the loop down
         time.sleep(0.2)
