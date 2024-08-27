0f116a0bbe21551ed5a84aa354508c0c44b4ec0b
Daniel Peace <danielpeace805@gmail.com>
2024-08-27 09:50:45 -0700

    - refactored "assign_apriltag.py" - refactored "prediction_timer" - changed type from float 64 to int32 in "Prediction_element.msg"

--- assign_apriltag.py
+++ assign_apriltag.py
@@ -5,24 +5,35 @@ from messages.msg import Assigned_apriltag
 # creating ROS topic and publisher
 publisher = rospy.Publisher('assigned_apriltag', Assigned_apriltag, queue_size=10)
 
+def system_print(s):
+    print("\u001b[34m[-] \u001b[0m" + s)
+
 if __name__ == "__main__":
     # initializing node
-    rospy.init_node('locate_correct_apriltag', anonymous=True)
-
-    # prompting user
-    print("system: Type \"s\" to start timer or q to quit:")
+    rospy.init_node('assign_apriltag', anonymous=True)
 
     while True:
-        # getting user choice
-        choice = input("-> ")
+        # prompting user
+        print("---------------------------------------------------------------------")
+        system_print("Enter the integer you would like to assign to the AprilTag")
+        print("    or type q to exit:")
 
-        # validating user input
-        if choice.isdigit():
-            assigned_apriltag = Assigned_apriltag()
-            assigned_apriltag.apriltag = int(choice)
-            publisher.publish(assigned_apriltag)
-        elif choice.upper() == 'Q':
-            print("system: Exiting...")
-            exit(0)
-        else:
-            print("system: invalid choice")
+        while True:
+            # getting user choice
+            print("---------------------------------------------------------------------")
+            choice = input("\u001b[34m -> \u001b[0m")
+            
+
+            # validating user input
+            if choice.isdigit():
+                assigned_apriltag = Assigned_apriltag()
+                assigned_apriltag.apriltag = int(choice)
+                publisher.publish(assigned_apriltag)
+                break
+            elif choice.upper() == 'Q':
+                print("---------------------------------------------------------------------")
+                system_print("Exiting...")
+                exit(0)
+            else:
+                print("---------------------------------------------------------------------")
+                system_print("Invalid choice")
--- prediction_timer.py
+++ prediction_timer.py
@@ -9,43 +9,54 @@ TIMER_LENGTH = 10
 # creating ROS topic and publisher
 publisher = rospy.Publisher('prediction_timer_status', Timer_status, queue_size=10)
 
+def system_print(s):
+    print("\u001b[34m[-] \u001b[0m" + s)
+
 def timer():
     # creating Timer_status message to indicate timer has started
     timer_status = Timer_status()
     timer_status.timer_status = True
 
     # publishing message
-    print("system: Publishing timer status message")
+    print("---------------------------------------------------------------------")
+    system_print("Publishing timer status message")
     publisher.publish(timer_status)
 
     # starting timer
-    print("system: Timer started for " + str(TIMER_LENGTH) + " seconds")
+    system_print("Timer started for " + str(TIMER_LENGTH) + " seconds")
     time.sleep(TIMER_LENGTH)
-    print("Timer ended")
+    system_print("Timer ended")
 
     # updating timer message to indicate timer has ended
     timer_status.timer_status = False
 
     # publishing message
-    print("system: Publishing timer status message")
+    system_print("Publishing timer status message")
     publisher.publish(timer_status)
 
 if __name__ == "__main__":
     # initializing node
     rospy.init_node('prediction_timer_node', anonymous=True)
 
-    # prompting user
-    print("system: Type \"s\" to start timer or q to quit:")
-
     while True:
-        # getting user choice
-        choice = input("-> ")
+        # prompting user
+        print("---------------------------------------------------------------------")
+        system_print(" Type \"s\" to start timer or q to quit:")
 
-        # validating user input
-        if choice.upper() == 'S':
-            timer()
-        elif choice.upper() == 'Q':
-            print("system: Exiting...")
-            exit(0)
-        else:
-            print("system: invalid choice")
+        while True:
+            # getting user choice
+            print("---------------------------------------------------------------------")
+            choice = input("\u001b[34m -> \u001b[0m")
+
+            # validating user input
+            if choice.upper() == 'S':
+                timer()
+                break
+            elif choice.upper() == 'Q':
+                print("---------------------------------------------------------------------")
+                system_print("Exiting...")
+                print("---------------------------------------------------------------------")
+                exit(0)
+            else:
+                print("---------------------------------------------------------------------")
+                system_print("Invalid choice")
