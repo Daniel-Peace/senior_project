6657e2102d56b3a4cd3d47391b3f20b17774e805
Daniel Peace <danielpeace805@gmail.com>
2024-10-09 20:13:27 -0700

    - created a custom label for titles

---
+++ desktop_app/components/TitleLabel.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+
+class TitleLabel(QLabel):
+    def __init__(self, text="", parent=None):
+        # instantiate parent object
+        super().__init__(text, parent)
+
+        # setting default properties
+        self.setAlignment(Qt.AlignCenter)
+        self.setStyleSheet(f"color: #ADB2BD;")
+        self.setFont(QFont("Arial", 14, QFont.Bold))
+
+    def updateText(self, text):
+        self.setText(text)
