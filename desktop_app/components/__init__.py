dac0e75b52ad56c4981c24ad0e9eb88fa5f71881
Daniel Peace <danielpeace805@gmail.com>
2024-10-10 18:14:08 -0700

    - added new LogWidget to __init__

--- desktop_app/components/__init__.py
+++ desktop_app/components/__init__.py
@@ -3,6 +3,7 @@ from .TitleLabel    import TitleLabel
 from .BodyLabel     import BodyLabel
 from .List          import List
 from .VideoView     import VideoView
+from .LogWidget     import LogWidget
 from .CardWidgetOld import CardWidgetOld
 from .CardWidget    import CardWidget
 from .MainWindow    import MainWindow
