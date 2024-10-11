f91b54aa34c8dc43d18d66d83a09a9b613cc97c7
Daniel Peace <danielpeace805@gmail.com>
2024-10-10 18:18:20 -0700

    - refactored

--- desktop_app/components/TitleLabel.py
+++ desktop_app/components/TitleLabel.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtGui import QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets    import QLabel
+from PyQt5.QtGui        import QFont
+from PyQt5.QtCore       import Qt
 
 class TitleLabel(QLabel):
     def __init__(self, text="", parent=None):
