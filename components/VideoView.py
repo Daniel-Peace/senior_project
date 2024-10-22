2c062cb8c3a5139f52a06de26ed9b69fa9dfeedb
Daniel Peace <danielpeace805@gmail.com>
2024-10-22 16:51:55 -0700

    - refactored VideoView.py

--- components/VideoView.py
+++ components/VideoView.py
@@ -3,22 +3,28 @@ from PyQt5.QtGui        import QPixmap, QImage
 from PyQt5.QtCore       import Qt
 
 class VideoView(QWidget):
+    # constructor
     def __init__(self, parent=None):
+        # instantiate parent object
         super().__init__(parent)
+
+        # creating a graphics view and scene
         self.view = QGraphicsView(self)
         self.scene = QGraphicsScene(self)
         self.view.setScene(self.scene)
 
-        # Remove borders and scrollbars
+        # removing borders and scrollbars
         self.view.setFrameShape(QGraphicsView.NoFrame)
         self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
-        # Initialize placeholder items
+        # initializing placeholder items
         self.pixmap_item = QGraphicsPixmapItem()
         self.scene.addItem(self.pixmap_item)
 
-    def update_image(self, numpy_array):        
+    # this function updates the iamge with each new frame received from the ROS usb camera node
+    def update_image(self, numpy_array): 
+        # getting data about image from numpy array       
         height, width, channels = numpy_array.shape
         bytes_per_line = channels * width
 
@@ -28,28 +34,21 @@ class VideoView(QWidget):
             4: QImage.Format_RGBA8888
         }
         
+        # checking if formatting is supported
         if channels not in format_map:
             raise ValueError(f"Unsupported number of channels: {channels}")
 
+        # creating a QImage from numpy array
         qimage = QImage(numpy_array.data, width, height, bytes_per_line, format_map[channels])
 
-        # Update QGraphicsView
+        # creating a QPixmap from the QImage and updating image
         pixmap = QPixmap.fromImage(qimage)
-        # pixmap = pixmap.scaled(monitors[0].width/2, monitors[0].width/2, Qt.KeepAspectRatio)
         self.pixmap_item.setPixmap(pixmap)
-        
         width = pixmap.width()
         height = pixmap.height()
 
-        # Set the scene rect to the size of the image
+        # setting view size based on image size
         self.scene.setSceneRect(0, 0, width, height)
-        
-        # Resize the QGraphicsView to fit the image size
         self.view.setFixedSize(width, height)
-        
-        # Resize the container widget to fit the new image size
         self.setFixedSize(self.view.size())
-
-    def resizeEvent(self, event):
-        super().resizeEvent(event)
         
