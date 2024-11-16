8a598d0214a961f12a8c5cca56f8604bac4fe9ec
Daniel Peace <danielpeace805@gmail.com>
2024-11-16 15:08:33 -0800

    - added shebangs to timer.py and send_reports.py -  refactored publish_test_reports.py and fixed typo in name

--- publish_test_reports.py
+++ publish_test_reports.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+# -------------------------------------------------------------------------------------------
+# Daniel Peace
+# CSUCI / Coordinated Robotics - DTC
+# -------------------------------------------------------------------------------------------
+# This program simply publishes test reports should you want to test "send_reports.py"
+# It allows the user to pick one of three reports to publish. The report is published
+# to it's corresponding topic. This would be one of the following:
+# - /critical_report
+# - /vitals_report
+# - /injury_report
+# -------------------------------------------------------------------------------------------
 
 # imports
 import rospy
@@ -6,20 +17,28 @@ from messages.msg import Critical_report
 from messages.msg import Injury_report
 from messages.msg import Vitals_report
 
-# constants
+# general constants
 TEAM_NAME = "coordinated robotics"
+
+# initializing ros node
+rospy.init_node('test_reports', anonymous=True)
 
 # initializing publishers
 critical_report_pub   = rospy.Publisher('critical_report', Critical_report, queue_size=10)
 vitals_report_pub     = rospy.Publisher('vitals_report', Vitals_report, queue_size=10)
 injury_report_pub     = rospy.Publisher('injury_report', Injury_report, queue_size=10)
 
-def print_bar():
-    print("------------------------------------------------------------------------------")
+# used for printing formatted system messages
+def system_print(s):
+    print("\u001b[34m[-] \u001b[0m" + s)
+
+# just helps reduce number of long print statements in code
+def print_horizontal_line():
+    print("---------------------------------------------------------------------")
 
 # send a test critical report
 def send_critical_report():
-    print_bar()
+    print_horizontal_line()
 
     # creating test report
     critical_report = Critical_report()
@@ -34,13 +53,15 @@ def send_critical_report():
     # publishing report
     critical_report_pub.publish(critical_report)
 
-    print("Published the following report:\n")
+    # printing the report that was publihsed for reference
+    system_print("Published the following report:")
+    print_horizontal_line()
     print(critical_report)
+    print_horizontal_line()
 
-    print_bar()
-
+# sends a test injury report
 def send_injury_report():
-    print_bar()
+    print_horizontal_line()
 
     # creating test report
     injury_report = Injury_report()
@@ -55,13 +76,15 @@ def send_injury_report():
     # publishing report
     injury_report_pub.publish(injury_report)
 
-    print("Published the following report:\n")
+    # printing the report that was publihsed for reference
+    system_print("Published the following report:")
+    print_horizontal_line()
     print(injury_report)
+    print_horizontal_line()
 
-    print_bar()
-
+# sends a test vitals report
 def send_vitals_report():
-    print_bar()
+    print_horizontal_line()
 
     # creating test report
     vitals_report = Vitals_report()
@@ -77,23 +100,28 @@ def send_vitals_report():
     # publishing report
     vitals_report_pub.publish(vitals_report)
 
-    print("Published the following report:\n")
+    # printing the report that was publihsed for reference
+    system_print("Published the following report:")
+    print_horizontal_line
     print(vitals_report)
+    print_horizontal_line()
 
-    print_bar()
-
-def test():
-    rospy.init_node('test_node', anonymous=True)
+# function to call other functions based on user_input
+def manage_publishers():
+    # looping until user quits
     while not rospy.is_shutdown():
         # prompting user
-        print("Choose which report you would like to send:")
+        print_horizontal_line()
+        system_print("Choose which report you would like to send:")
         print("\ta. Critical Report")
         print("\tb. Inury Report")
         print("\tc. Vitals Report")
+        print_horizontal_line()
         while True:
             # prompting user
             choice = input("\u001b[34m-> \u001b[0m")
 
+            # checking user choice
             if choice == 'a' or choice == 'A':
                 send_critical_report()
             elif choice == 'b' or choice == 'B':
@@ -101,13 +129,15 @@ def test():
             elif choice == 'c' or choice == 'C':
                 send_vitals_report()
             elif choice == 'q' or choice == 'Q':
-                print("Exiting...")
-                break
+                print_horizontal_line()
+                system_print("Exiting...")
+                print_horizontal_line()
+                exit(0)
             else:
-                print("\u001b[34m-> \u001b[0m \u001b[31mInvalid choice...\u001b[0m")
+                print_horizontal_line()
+                system_print("Invalid choice...")
+                print_horizontal_line()
 
+# "main function" of the program
 if __name__ == '__main__':
-    try:
-        test()
-    except rospy.ROSInterruptException:
-        pass
+    manage_publishers()
--- send_reports.py
+++ send_reports.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -------------------------------------------------------------------------------------------
 # Daniel Peace
 # CSUCI / Coordinated Robotics - DTC
